6d467b6df6ad9ee91811002ab4a06a6280a79257
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-26 22:22:09 -0700

    added comments for the better visualisation

--- src/MinimumCostOfRopes.java
+++ src/MinimumCostOfRopes.java
@@ -2,12 +2,19 @@ import java.util.PriorityQueue;
 
 public class MinimumCostOfRopes {
 
+
+    //add the elements in such a way so that the cost(addition) becomes the least
     static long minimumCostOfRopes(long []arr, int n){
         long cost = 0;
+        //add the all the elements in min heap
         PriorityQueue<Long> pq = new PriorityQueue<>();
         for(int i = 0; i<n; i++){
             pq.add(arr[i]);
         }
+        //remove the smallest two elements and store them in a variable
+        //then add that variable in minHeap and repeat the process,
+        // in the mean time store that sum in cost and update it with the new ones everytime
+
         while(pq.size()>1){
             long a = pq.poll();
             long b = pq.poll();
