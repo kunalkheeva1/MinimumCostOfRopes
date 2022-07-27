3e6bec98aed818931d4b4bc601966747b0136ea6
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-26 22:17:54 -0700

    added the elements in the priority queue

--- src/MinimumCostOfRopes.java
+++ src/MinimumCostOfRopes.java
@@ -1,6 +1,22 @@
+import java.util.PriorityQueue;
+
 public class MinimumCostOfRopes {
 
-    
+    static long minimumCostOfRopes(long []arr, int n){
+        long cost = 0;
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        for(int i = 0; i<n; i++){
+            pq.add(arr[i]);
+        }
+        while(pq.size()>1){
+            long a = pq.poll();
+            long b = pq.poll();
+
+            long c = a+b;
+
+
+        }
+    }
     public static void main(String[] args) {
 
     }
